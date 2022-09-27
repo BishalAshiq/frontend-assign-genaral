edf90adf1b7755a27e7517a97e2515a17c7bd58a
BishalAshiq <bishaltheboss20@gmail.com>
2022-09-27 19:54:05 +0600

    added more items

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,43 +1,62 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import p1 from "../../../images/banner/p1.jpg";
+import p2 from "../../../images/banner/p2.jpg";
+import p3 from "../../../images/banner/p3.jpg";
+import p4 from "../../../images/banner/p4.jpg";
+
 
 const Banner = () => {
   return (
-    <div>
+    <div style={{margin: 0 }}>
       <Carousel fade>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            src={p1}
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3 style={{fontSize: 48}}>Programming</h3>
+            <p style={{fontSize: 28}}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            src={p2}
             alt="Second slide"
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3 style={{fontSize: 48, color: "black"}}>Time Management</h3>
+            <p style={{fontSize: 28, color: "black"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src={p3}
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
+            <h3 style={{fontSize: 48, color: "black"}}>Team Work</h3>
+            <p style={{fontSize: 28, color: "black"}}>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={p4}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3 style={{fontSize: 48, color: "White"}}>Technical support</h3>
+            <p style={{fontSize: 28, color: "white"}}>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Caption>
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Navigation from '../Nav/Navigation';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <Banner></Banner>
         </div>
     );
 };
