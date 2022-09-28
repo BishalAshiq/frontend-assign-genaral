3f1cb6878fb28aed7bf0180c4657f77dc88eef81
BishalAshiq <bishaltheboss20@gmail.com>
2022-09-28 19:10:23 +0600

    updated more items

--- src/Components/CreatingActivity/ActivePageOne.js
+++ src/Components/CreatingActivity/ActivePageOne.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "./ActivePageOne.css";
 import { NavLink } from "react-router-dom";
-import CapabilitiesInp from "./CapabilitiesInp";
+import { useState } from "react";
 
 const ActivePageOne = () => {
+  const[inputData, setInputData]= useState('');
   return (
+    <>
     <div className="activePageOne">
       <div className="activePage-header">
         <NavLink to="/">
@@ -29,10 +31,33 @@ const ActivePageOne = () => {
       <div>
         <h6 className="cap-h6">CAPABILITIES</h6>
       </div>
-      <div>
-        <CapabilitiesInp />
+      <div className="cap">
+        <input
+          type="text"
+          placeholder="Enter Capability..."
+          className="text-cap"
+        />
+        <i
+          className="fa-solid fa-circle-plus add-btn"
+          title="Add-Items"
+          style={{ marginTop: 30, fontSize: 40, cursor:"pointer" }}
+        ></i>
+      </div>
+      <div className="Add-Items">
+        <div className="Each-Items">
+          <h5>Capabilities</h5>
+          <i
+            class="fa-solid fa-trash-alt add-btn"
+            title="Delete-Items"
+            style={{ fontSize: 20, marginLeft: 40, marginTop: 5, cursor:"pointer" }}
+          ></i>
+        </div>
+        <div className="del-items">
+            <Button variant="outline-dark">Remove All</Button>
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
